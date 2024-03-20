2059e672290137ef7506318ff8a99a3f4ff09b4e
Maria Punina <puninama@mail.ru>
2024-03-21 01:21:58 +0500

    input

--- Program.cs
+++ Program.cs
@@ -12,3 +12,31 @@
 // [“Hello”, “2”, “world”, “:-)”] → [“2”, “:-)”]
 // [“1234”, “1567”, “-2”, “computer science”] → [“-2”]
 // [“Russia”, “Denmark”, “Kazan”] → []
+
+string[] array = new string[] { "Hello", "2" };
+string[] arrayNew = new string[array.Length];
+
+void TaskArr(string[] array, string[] arrayNew)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; ++i)
+    {
+        if (array[i].Length <= 3)
+        {
+            arrayNew[count] = array[i];
+            count++;
+        }
+    }
+}
+
+void PrintArray(string[] array)
+{ 
+    for(int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+
+TaskArr(array, arrayNew);
+PrintArray(arrayNew);
